83f4ed1f759c65ba9a46bf9639340156abc4fb6a
samumu1234 <75941684+samumu1234@users.noreply.github.com>
2024-10-26 23:29:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -38,8 +38,5 @@ document.addEventListener("scroll", function() {
 const carouselInner = document.querySelector('.carousel-inner');
 const logos = document.querySelectorAll('.carousel-logo');
 const totalLogos = logos.length;
-document.getElementById('navbar-toggle').addEventListener('click', function() {
-    const menu = document.getElementById('navbar-menu');
-    menu.classList.toggle('active'); // Alternar la clase 'active' para mostrar/ocultar el menú
-});
+
 
