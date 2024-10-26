cdefc80bd74913ed721a08b5e864b26d26cf72d7
samumu1234 <75941684+samumu1234@users.noreply.github.com>
2024-10-26 18:38:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Código para interacciones adicionales
 });
+document.addEventListener("scroll", function() {
+    const experienceSection = document.querySelector('.experience');
+    const sectionPosition = experienceSection.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.2;
+
+    if (sectionPosition < screenPosition) {
+        experienceSection.style.opacity = 1; // Hacer visible
+        experienceSection.style.transform = 'translateY(0)'; // Regresar a la posición original
+    }
+});
